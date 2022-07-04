16b7fd4deaf1711cabb959b67c12984e054ca82c
Diego Luna <slowkingvii@gmail.com>
2022-07-04 15:53:02 -0500

    Create add function for Book List

--- script.js
+++ script.js
@@ -1,21 +1,42 @@
-const books = [
-    {
-        title: 'The kite runner',
-        author: 'Khalid',
-    },
-];
+const books = [];
 
-books.forEach((element, i) => {
+class Book {
+    constructor(title, author) {
+        this.title = title;
+        this.author = author;
+    }
+}
+
+const refreshBookList = () => {
     const booksSection = document.getElementById('books-section');
+    let listItems = "";
+    books.forEach((element, i) => {
+        const book = `
+        <li>
+                    <p>${books[i].title}</p>
+                    <p>${books[i].author}</p>
+                    <button type="button">Remove</button>
+        </li>
+        `;
 
-    const book = `
-    <li>
-                <p>${books[i].title}</p>
-                <p>${books[i].author}</p>
-                <button type="button">Remove</button>
-     </li>
-    `;
+        listItems += book;
 
-    booksSection.innerHTML += book;
+    });
 
-});
+    booksSection.innerHTML = listItems;
+}
+refreshBookList();
+
+const addBook = () => {
+    const book = {};
+    book.title = titleInput.value;
+    book.author = authorInput.value;
+    books.push(book);
+    refreshBookList();
+}
+
+const titleInput = document.getElementById('title');
+const authorInput = document.getElementById('author');
+const addButton = document.getElementById('submit');
+
+addButton.addEventListener('click', addBook);
