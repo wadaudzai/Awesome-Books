08ad609488e7feb5ccbf7085e5ae09a054fc7c71
Diego Luna <slowkingvii@gmail.com>
2022-07-04 17:38:11 -0500

    Install and fix linters

--- script.js
+++ script.js
@@ -1,25 +1,11 @@
 let books = [];
 if (localStorage.books) books = JSON.parse(localStorage.getItem('books'));
 
-class Book {
-    constructor(title, author) {
-        this.title = title;
-        this.author = author;
-    }
-}
-
-const removeBook = (event) => {
-    const indexBook = event.currentTarget.dataset.index;
-    books.splice(parseInt(indexBook),1);
-    refreshBookList();
-    localStorage.setItem('books', JSON.stringify(books));
-}
-
 const refreshBookList = () => {
-    const booksSection = document.getElementById('books-section');
-    let listItems = "";
-    books.forEach((element, i) => {
-        const book = `
+  const booksSection = document.getElementById('books-section');
+  let listItems = '';
+  books.forEach((element, i) => {
+    const book = `
         <li>
                     <p>${books[i].title}</p>
                     <p>${books[i].author}</p>
@@ -27,31 +13,31 @@ const refreshBookList = () => {
         </li>
         `;
 
-        listItems += book;
+    listItems += book;
+  });
 
-    });
+  booksSection.innerHTML = listItems;
 
-    booksSection.innerHTML = listItems;
-    document.querySelectorAll('.remove').forEach((element) => 
-    element.addEventListener('click', removeBook));
-}
-refreshBookList();
-
-const addBook = () => {
-    const book = {};
-    book.title = titleInput.value;
-    book.author = authorInput.value;
-    books.push(book);
+  document.querySelectorAll('.remove').forEach((element) => element.addEventListener('click', (event) => {
+    const indexBook = event.currentTarget.dataset.index;
+    books.splice(parseInt(indexBook, 10), 1);
     refreshBookList();
     localStorage.setItem('books', JSON.stringify(books));
-}
+  }));
+};
+refreshBookList();
 
 const titleInput = document.getElementById('title');
 const authorInput = document.getElementById('author');
 const addButton = document.getElementById('submit');
 
+const addBook = () => {
+  const book = {};
+  book.title = titleInput.value;
+  book.author = authorInput.value;
+  books.push(book);
+  refreshBookList();
+  localStorage.setItem('books', JSON.stringify(books));
+};
+
 addButton.addEventListener('click', addBook);
-
-// remove button 
-
-
