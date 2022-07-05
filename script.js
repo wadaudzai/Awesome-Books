25cdc1dc2026fd21969268d243c625132ef0bd30
Diego Luna <slowkingvii@gmail.com>
2022-07-05 16:06:55 -0500

    Add remove function and localStorage

--- script.js
+++ script.js
@@ -8,7 +8,7 @@
 //     const book = `
 //         <li class="single-book">
 //                     <p>"${books[i].title}" by ${books[i].author}</p>
-//                     <button type="button" class="remove" data-index = "${i}">Remove</button>
+//                     <button type="button" class="remove" data-index = "${i}" onclick="removeButton(event)">Remove</button>
 //         </li>
 //         `;
 
@@ -18,10 +18,7 @@
 //   booksSection.innerHTML = listItems;
 
 //   document.querySelectorAll('.remove').forEach((element) => element.addEventListener('click', (event) => {
-//     const indexBook = event.currentTarget.dataset.index;
-//     books.splice(parseInt(indexBook, 10), 1);
-//     refreshBookList();
-//     localStorage.setItem('books', JSON.stringify(books));
+//     
 //   }));
 // };
 // refreshBookList();
@@ -44,9 +41,17 @@ const titleInput = document.getElementById('title');
 const authorInput = document.getElementById('author');
 const addButton = document.getElementById('submit');
 
+class Book {
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
+  }
+}
+
 class Booklist {
 	constructor() {
 		this.books = [];
+    if (localStorage.books) this.books = JSON.parse(localStorage.getItem('books'));
 	}
 	refreshBookList() {
 		let listItems = '';
@@ -54,7 +59,7 @@ class Booklist {
 		this.books.forEach((element, i) => {
 			const book = `<li class="single-book">
                                  <p>"${this.books[i].title}" by ${this.books[i].author}</p>
-                                 <button type="button" class="remove" data-index = "${i}">Remove</button>
+                                 <button type="button" class="remove" data-index = "${i}" onclick="removeButton(event)">Remove</button>
                      </li>`;
 
 			listItems += book;
@@ -63,20 +68,25 @@ class Booklist {
 
 		booksSection.innerHTML = listItems;
 	}
-
 }
 
 const booklist = new Booklist;
 booklist.refreshBookList();
 
 function addBook() {
-	const book = {};
-	book.title = titleInput.value;
-	book.author = authorInput.value;
+	const book = new Book(titleInput.value, authorInput.value);
 	booklist.books.push(book);
 	booklist.refreshBookList();
+  localStorage.setItem('books', JSON.stringify(booklist.books));
+  titleInput.value = '';
+  authorInput.value = '';
+}
+
+function removeButton(event) {
+  const indexBook = event.currentTarget.dataset.index;
+  booklist.books.splice(parseInt(indexBook, 10), 1);
+  booklist.refreshBookList();
+  localStorage.setItem('books', JSON.stringify(booklist.books));
 }
 
 addButton.addEventListener('click', addBook);
-
-
